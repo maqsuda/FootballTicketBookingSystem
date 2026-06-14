cec2352ddb3f105abd45d6fee29e04133b985e95
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-06-14 13:06:18 +0600

    added match table

--- query.sql
+++ query.sql
@@ -13,3 +13,36 @@ INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
+DROP TABLE IF EXISTS Matches;
+CREATE TABLE Matches(
+  match_id INT PRIMARY KEY,
+  fixture TEXT,
+  tournament_category TEXT,
+  base_ticket_price INT CHECK (base_ticket_price>0),
+  match_status VARCHAR(20) CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+)
+
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+
+DROP TABLE IF EXISTS Bookings;
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    match_id INT REFERENCES matches(match_id),
+    seat_number VARCHAR(20) ,
+    payment_status TEXT CHECK (payment_status IN('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost INT CHECK (total_cost>0)
+   );
+
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
