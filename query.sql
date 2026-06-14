fc7e25c52d4d310d1d1de94f99aa0ab3ca1204a4
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-06-14 13:07:03 +0600

    added query

--- query.sql
+++ query.sql
@@ -46,3 +46,55 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+
+SELECT match_id,fixture,base_ticket_price
+  FROM matches
+WHERE match_status ='Available'
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT user_id,full_name,email
+  FROM users
+WHERE full_name Ilike 'tanvir%' 
+OR full_name Ilike '%haque%'
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+
+SELECT booking_id,user_id,match_id,coalesce(payment_status,'Action Required') 
+  FROM bookings
+WHERE payment_status is null
+
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+  
+SELECT booking_id,full_name,fixture,total_cost 
+  FROM users 
+  JOIN bookings ON users.user_id=bookings.user_id
+  JOIN matches ON matches.match_id=bookings.match_id
+ 
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+SELECT users.user_id,full_name,booking_id
+FROM users LEFT JOIN bookings ON users.user_id=bookings.user_id
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+SELECT booking_id,matches.match_id,total_cost
+FROM bookings JOIN matches 
+ON bookings.match_id=matches.match_id
+WHERE total_cost>(SELECT AVG(total_cost) FROM bookings)
+
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+SELECT match_id,fixture,base_ticket_price
+  FROM matches
+ORDER BY base_ticket_price DESC LIMIT 2 OFFSET 1 
+
+
