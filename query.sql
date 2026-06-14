0831264d59702cdb9cebc8ceebf3444006a646ad
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-06-14 13:05:03 +0600

    added users

---
+++ query.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users(
+  user_id SERIAL PRIMARY KEY,
+  full_name TEXT,
+  email VARCHAR(30) UNIQUE NOT NULL,
+  role VARCHAR(20) ,
+  phone_number VARCHAR(30)
+)
+
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
